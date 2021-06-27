3d96cc0bba2c7cbd6a9022b05c94aa3fcd25f0fa
KBurgmann <kevin.burgmann@gatgetron.net>
2021-06-27 20:15:33 +0200

    Added publishing to maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.10"
+    `maven-publish`
+    java
 }
 
 group = "id.walt.servicematrix"
@@ -7,9 +9,38 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven {
+        setUrl("https://maven.pkg.jetbrains.space/walt-id/p/servicematrix/maven")
+        credentials {
+            username = "$username"
+            password = "$password"
+        }
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.10")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "id.walt.servicematrix"
+            artifactId = "servicematrix"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            setUrl("https://maven.pkg.jetbrains.space/walt-id/p/servicematrix/maven")
+            credentials {
+                username = "$username"
+                password = "$password"
+            }
+        }
+    }
 }
